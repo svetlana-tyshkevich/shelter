d2b99d2ad567dfe6cb519868fb07a8974026daf3
Svetlana Tyshkevich <panfilova.ss@gmail.com>
2020-10-25 09:59:31 +0200

    feat: add slider for main page

--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -13,3 +13,64 @@ const openBurgerMenu = (e) => {
 
 burger.addEventListener('click', openBurgerMenu);
 overlay.addEventListener('click', openBurgerMenu);
+
+
+// SLIDER
+const petsCards = document.querySelector('.pets__cards');
+const petsButtons = document.querySelectorAll('.pets__gallery > button');
+const cards = document.querySelectorAll('.card');
+let pets = [];
+
+fetch('../../assets/pets.json').then(res => res.json()).then(list => {
+    pets = list;
+    
+    
+    let prevCards = [];
+    const sliderRandom = () => {
+        let currentCards = [];
+        petsCards.innerHTML = '';
+        let n = 3;
+
+        while (currentCards.length < n) {
+            let randomPet = pets[Math.floor(Math.random() * pets.length)];
+            if (!currentCards.includes(randomPet) && !prevCards.includes(randomPet)) {
+                currentCards.push(randomPet);
+            }
+        }
+
+        currentCards.forEach( item => {petsCards.innerHTML += `<div class="card" id='${item.name}'>
+                                    <img src='${item.img}' alt='${item.name}' class="card__photo">
+                                    <span class="card__name">${item.name}</span>
+                                    <button class="button button_bordered">Learn more</button>
+                                </div>`})
+        prevCards = currentCards;
+        currentCards = [];
+    }
+    
+   // POPUP 
+    const showPopup = (e) => {
+        console.log(e.target)
+        // pets.forEach(item => {
+        //     if (item.name === e.target.id) {
+        //         popup.innerHTML += `${pets.indexOf(item)}`
+        //     }
+        // })
+
+        // let currentPetNum = 
+    
+}
+
+    petsButtons.forEach(item => item.addEventListener('click', sliderRandom));
+    cards.forEach(item => item.addEventListener('click', showPopup()));
+
+
+    sliderRandom();
+    
+});
+
+
+
+
+
+
+
